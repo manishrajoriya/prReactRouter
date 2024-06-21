8258d13b2a53f2a1f7a87bff137ab585125de200
manish <gkrajoriya3@gmail.com>
2024-06-21 07:12:50 +0530

    later uploaded

---
+++ src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Home from './Home/Home'
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
